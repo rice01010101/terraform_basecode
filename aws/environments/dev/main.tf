7481576bd04b0c6f85b568521c6817de433ed2c0
rice01010101 <rice01010101@example.com>
2024-06-26 21:50:01 +0900

    init

--- aws/environments/dev/main.tf
+++ aws/environments/dev/main.tf
@@ -8,14 +8,14 @@ data "aws_availability_zones" "available" {}
 ################################################################################
 # EC2
 ################################################################################
-# resource "aws_instance" "web" {
-#   ami           = "ami-039e8f15ccb15368a" # Amazon Linux 2023 AMI 2023.3.20240304.0 x86_64 HVM kernel-6.1
-#   instance_type = "t3.micro"
+resource "aws_instance" "web" {
+  ami           = "ami-039e8f15ccb15368a" # Amazon Linux 2023 AMI 2023.3.20240304.0 x86_64 HVM kernel-6.1
+  instance_type = "t3.micro"
 
-#   tags = {
-#     Name = "HelloWorld"
-#   }
-# }
+  tags = {
+    Name = "HelloWorld"
+  }
+}
 
 locals {
   tst = 13
